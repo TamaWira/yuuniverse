0eac781ecb1c38184a22585099ea5681b8b2a417
Bayu <iputubayuadhyawiratama@Is-MacBook-Air-2.local>
2025-04-24 21:12:35 +0800

    feat: update hero content

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -13,13 +13,17 @@ export default function Hero() {
       />
 
       {/* Gradient Overlay */}
-      <div className="z-10 absolute inset-0 bg-gradient-to-b from-white/60 dark:from-black via-white/20 dark:via-black/20 to-transparent h-[80%] pointer-events-none" />
+      <div className="z-10 absolute inset-0 bg-gradient-to-b from-white dark:from-black via-white/20 dark:via-black/20 to-transparent h-[80%] pointer-events-none" />
 
+      {/* Content */}
       <div className="top-10 lg:top-20 lg:left-1/2 z-20 absolute text-center lg:-translate-x-1/2">
+        <p className="text-xl">Hi there, I&apos;m Bayu</p>
         <h1 className="font-bold text-2xl lg:text-4xl">
-          I&apos;m Bayu, and welcome to YuuNiverse
+          Welcome to my YuuNiverse
         </h1>
-        <p className="lg:text-xl">where magic begins</p>
+        <p className="lg:text-xl">
+          Where curiosity meets clean code, and every click tells a story.
+        </p>
         <button className="hover:bg-black dark:hover:bg-white mt-5 px-3 py-1 border dark:border-2 dark:border-white border-black rounded-full hover:text-white dark:hover:text-black transition-all">
           Contact Me
         </button>
