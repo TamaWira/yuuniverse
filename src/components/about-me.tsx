b5e01bb9494738126fd97510889212b52ce1e783
Bayu <asusrog20012001@gmail.com>
2025-04-26 18:01:07 +0800

    feat: add animation to about me section

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -1,8 +1,33 @@
+"use client";
+
+import { motion, Variants } from "framer-motion";
+
+const aboutMeVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      ease: "easeOut",
+    },
+  },
+};
+
 export default function AboutMe() {
   return (
     <section id="about-me" className="relative flex items-center h-full">
       {/* <SectionHeader>About Me</SectionHeader> */}
-      <div className="lg:flex-1 lg:px-32 font-light text-md lg:text-3xl text-center">
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ amount: "all", once: true, margin: "0px 0px -100px 0px" }}
+        variants={aboutMeVariants}
+        className="lg:flex-1 lg:px-32 font-light text-md lg:text-3xl text-center"
+      >
         <p>
           I&apos;m a software engineer with{" "}
           <span className="lg:font-medium font-bold text-[#576856] dark:text-[#90ab8f] underline">
@@ -31,21 +56,35 @@ export default function AboutMe() {
           experimenting with new UI libraries like Shadcn, I&apos;m always down
           to learn something new and make things better.
         </p>
-      </div>
+      </motion.div>
 
-      <div className="top-[10%] lg:top-[10%] -left-[2%] lg:left-[5%] -z-10 absolute">
+      {/* Floating Icons 1 */}
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ amount: "all", once: true, margin: "0px 0px -100px 0px" }}
+        variants={aboutMeVariants}
+        className="top-[10%] lg:top-[10%] -left-[2%] lg:left-[5%] -z-10 absolute"
+      >
         <div className="-z-10 inset-0 bg-gray-700 blur-2xl lg:blur-3xl rounded-full w-20 lg:w-32 h-20 lg:h-32" />
         <div className="top-1/2 left-1/2 absolute flex justify-center items-center bg-gray-500/10 border rounded-lg w-10 h-10 -rotate-30 -translate-x-1/2 -translate-y-1/2">
           <div className="flex justify-center items-center text-xl">⚛️</div>
         </div>
-      </div>
+      </motion.div>
 
-      <div className="top-[75%] lg:top-[70%] -right-[3%] lg:right-[3%] -z-10 absolute">
-        <div className="-z-10 inset-0 bg-gray-700 blur-3xl rounded-full w-32 h-32" />
+      {/* Floating Icons 2 */}
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ amount: "all", once: true, margin: "0px 0px -100px 0px" }}
+        variants={aboutMeVariants}
+        className="top-[75%] lg:top-[70%] -right-[3%] lg:right-[3%] -z-10 absolute"
+      >
+        <div className="-z-10 inset-0 bg-gray-700 blur-2xl lg:blur-3xl rounded-full w-20 lg:w-32 h-20 lg:h-32" />
         <div className="top-1/2 left-1/2 absolute flex justify-center items-center bg-gray-500/10 border rounded-lg w-10 h-10 rotate-30 -translate-x-1/2 -translate-y-1/2">
           <div className="flex justify-center items-center text-xl">⚛️</div>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
