5cd12adadb47b17dbb49048c8a342d16b5b4e817
Bayu Adhya <asusrog20012001@gmail.com>
2025-06-05 19:25:09 +0800

    feat: scaffold the layout of skills images

--- src/components/features/skills/skill-list.tsx
+++ src/components/features/skills/skill-list.tsx
@@ -31,11 +31,10 @@ export function SkillList() {
       </div>
 
       {/* Skills */}
-      <div className="gap-2 md:gap-5 grid grid-cols-2 md:grid-cols-4">
+      <div className="gap-2 md:gap-5 grid grid-cols-3 md:grid-cols-4">
         {skills[selectedSkill].tools.map((skill) => (
-          <AspectRatio
-            className="border rounded-lg"
-            ratio={3 / 2}
+          <div
+            className="flex flex-col py-3 border rounded-lg aspect-square md:aspect-[3/2] overflow-hidden"
             key={skill.name}
           >
             {/* TODO: Implement tools image  */}
@@ -46,8 +45,19 @@ export function SkillList() {
               height={100}
               className="border"
             /> */}
+            <div className="relative flex flex-1 justify-end items-center w-full">
+              <Image
+                src="/react.svg"
+                alt="React"
+                // width={50}
+                // height={50}
+                fill
+                className="object-contain"
+                sizes="100vw"
+              />
+            </div>
             <p className="font-extralight text-sm md:text-lg">{skill.name}</p>
-          </AspectRatio>
+          </div>
         ))}
       </div>
     </div>
