c322a0d1c903826ca9bf8249b26e5a34923abb97
Bayu Adhya <asusrog20012001@gmail.com>
2025-06-05 20:17:39 +0800

    feat: add skills images

--- src/components/features/skills/skill-list-items.tsx
+++ src/components/features/skills/skill-list-items.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import { skills } from "@/lib/constants";
+import Image from "next/image";
 
 interface SkillListItemsProps {
   selectedSkill: number;
@@ -10,16 +10,15 @@ export function SkillListItems({ selectedSkill }: SkillListItemsProps) {
     <div className="gap-2 md:gap-5 grid grid-cols-3 md:grid-cols-4">
       {skills[selectedSkill].tools.map((skill) => (
         <div
-          className="flex flex-col py-3 border rounded-lg aspect-square md:aspect-[3/2] overflow-hidden"
+          className="flex flex-col py-3 border rounded-lg aspect-square md:aspect-[3/2]"
           key={skill.name}
         >
-          <div className="relative flex flex-1 justify-end items-center w-full">
+          <div className="relative flex flex-1 justify-end items-center p-3 w-full h-full">
             <Image
-              src="/react.svg"
-              alt="React"
+              src={skill.image}
+              alt={skill.name}
               fill
-              className="object-contain"
-              sizes="100vw"
+              className="p-4 object-contain"
             />
           </div>
           <p className="font-extralight text-sm md:text-lg">{skill.name}</p>
