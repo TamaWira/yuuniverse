7f80c7f468d3a12b379780caf4f39e32ad1ec40e
Bayu Adhya <asusrog20012001@gmail.com>
2025-11-16 20:46:27 +0700

    fix: fix layout by adding leading to both text line

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,11 @@
 export default function Logo() {
   return (
-    <div className="font-abhaya text-lg italic">
+    <div className="font-abhaya md:text-lg italic">
       <p className="font-extralight">
-        I Putu <span className="font-bold text-2xl">Bayu</span>
+        I Putu{" "}
+        <span className="font-bold text-xl leading-1 md:text-2xl m-0 p-0">
+          Bayu
+        </span>
       </p>
       <p className="font-extralight leading-1 m-0 p-0">Adhya Wiratama</p>
     </div>
